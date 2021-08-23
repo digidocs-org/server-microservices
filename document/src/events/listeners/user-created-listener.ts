1ac1a56db98b2dbe65ec9d3809563ae707ee862b
Naman Singh <naman.icebed@gmail.com>
2021-08-23 14:55:37 +0530

    added create user in document service

--- document/src/events/listeners/user-created-listener.ts
+++ document/src/events/listeners/user-created-listener.ts
@@ -1,6 +1,7 @@
 import { Subjects, Listener, UserCreatedEvent } from '@digidocs/guardian';
 import { queueGroupName } from 'document-service/types'
 import { Message } from 'node-nats-streaming';
+import User from 'document-service/models/user'
 
 export class CreateUserListener extends Listener<UserCreatedEvent>{
     subject: Subjects.UserCreated = Subjects.UserCreated;
@@ -8,7 +9,19 @@ export class CreateUserListener extends Listener<UserCreatedEvent>{
 
     async onMessage(data: UserCreatedEvent['data'], msg: Message) {
         try {
-            console.log(data)
+            const user = await User.create({
+                _id: data.id,
+                email: data.email,
+                firstname: data.firstname,
+                lastname: data.lastname,
+                mobile: data.mobile,
+                isBlocked: data.isBlocked,
+                isPremium: data.isPremium,
+                profileImage: data.profileImage,
+                notificationId: data.notificationId,
+                deviceId: data.deviceId,
+                version: data.version,
+            })
             msg.ack()
         } catch (error) {
             console.log(error)
