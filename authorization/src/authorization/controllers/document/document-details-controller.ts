a953fe8f1fe746794bee59452ff5543de19250c1
Yash Saini <yash.saini@airtel.com>
2021-10-29 01:20:25 +0530

    Bug Fixes

--- authorization/src/authorization/controllers/document/document-details-controller.ts
+++ authorization/src/authorization/controllers/document/document-details-controller.ts
@@ -1,6 +1,7 @@
 import { IDocumentActions } from 'authorization-service/models/Actions';
 import { IDocument } from 'authorization-service/models/Document';
 import DocumentUserMap from 'authorization-service/models/DocumentUserMap';
+import User from 'authorization-service/models/User';
 import { Request, Response } from 'express';
 
 export const documentDetailsController = async (
@@ -10,11 +11,14 @@ export const documentDetailsController = async (
   const documentId = req.params.documentId;
   let document: any = req.docUserMap?.document as IDocument;
   const action = req.docUserMap?.action as IDocumentActions;
+  const owner = await User.findById(document.userId);
 
   document = document.toJSON({
     transform: (doc: any, ret: any) => {
       delete ret.documentId;
       delete ret.publicKeyId;
+      ret.ownerName = `${owner?.firstname} ${owner?.lastname}`;
+      ret.ownerEmail = owner?.email;
     },
   });
 
@@ -31,6 +35,7 @@ export const documentDetailsController = async (
       recipientEmail: action.recipientEmail,
       status: action.actionStatus,
       recipientName: action.recipientName,
+      fields: action.fields,
     };
   });
 
