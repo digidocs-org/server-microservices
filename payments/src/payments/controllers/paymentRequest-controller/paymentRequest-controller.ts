a8282b07b3f4442c6b6d6e56478de61c60eb16f9
Naman Singh <naman.icebed@gmail.com>
2021-11-22 22:07:38 +0530

    updated payments

--- payments/src/payments/controllers/paymentRequest-controller/paymentRequest-controller.ts
+++ payments/src/payments/controllers/paymentRequest-controller/paymentRequest-controller.ts
@@ -1,8 +1,31 @@
 import { Request, Response } from "express";
+import Razorpay, { } from 'razorpay'
 
 export const paymentRequest = (req: Request, res: Response) => {
-    const { userId, amount, currency } = req.body
+    const { user, amount, currency, token } = req.body
 
-    
-    
+    const { name, email, phoneNo } = user
+
+    const razorpay = new Razorpay({
+        key_id: process.env.RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_KEY_SECRET
+    })
+
+    const paymentOptions = {
+        amount,
+        currency
+    }
+    razorpay.orders.create(paymentOptions, function (err: any, order: any) {
+        const orderId: string = order.id
+
+        res.render('paymentRequest.ejs', {
+            orderId,
+            amount,
+            currency,
+            name,
+            email,
+            phoneNo,
+            token
+        })
+    });
 }
