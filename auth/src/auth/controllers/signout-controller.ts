0cd99ac1ca7051d3f75f8784abddd31cf5f02a51
Yash Saini <yash.saini@airtel.com>
2022-03-13 21:44:52 +0530

    Logout API

--- auth/src/auth/controllers/signout-controller.ts
+++ auth/src/auth/controllers/signout-controller.ts
@@ -1,7 +1,9 @@
+import { COOKIE_OPTIONS } from '@digidocs/guardian';
 import { Request, Response } from 'express';
 
 export const signout = (req: Request, res: Response) => {
-  res.clearCookie('session');
-  res.clearCookie('refreshToken');
+  res.clearCookie('session', COOKIE_OPTIONS);
+  res.clearCookie('refreshToken', COOKIE_OPTIONS);
+  // req.session.destroy();
   return res.send({ succes: true, message: 'Logged out successfully!' });
 };
