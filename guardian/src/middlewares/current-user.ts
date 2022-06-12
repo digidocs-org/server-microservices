961d0f15a45b4744d3d44a1c762069a1aeced8e1
Naman Singh <naman.icebed@gmail.com>
2022-06-12 20:46:06 +0530

    removed hardcoded cookie

--- guardian/src/middlewares/current-user.ts
+++ guardian/src/middlewares/current-user.ts
@@ -23,7 +23,6 @@ export const currentUser = (
   next: NextFunction
 ) => {
 
-  req.cookies['session'] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjNmNjllYmJhOWRjZGZmYzQzNjYzMyIsImlhdCI6MTY1MDcxODM2NywiZXhwIjoxNzM5MjIxMzY3fQ.LysZKDDMTfs6DvAwAsRSMtxsFRSXxuxxvhv7-Xel7tk"
   const token = req.cookies['session'] ?? req.header('token') ?? req.body.token;
 
   try {
