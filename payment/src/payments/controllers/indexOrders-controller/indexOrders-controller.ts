bace035a3303f4edee0f9d6625a68aa1ecf2f265
Naman Singh <naman.icebed@gmail.com>
2022-07-05 11:38:13 +0530

    refactored code

--- payment/src/payments/controllers/indexOrders-controller/indexOrders-controller.ts
+++ payment/src/payments/controllers/indexOrders-controller/indexOrders-controller.ts
@@ -1,10 +1,17 @@
+import { BadRequestError } from '@digidocs/guardian';
 import { Request, Response } from 'express'
 import PaymentOrders from "payments-service/models/payment-orders";
 
 export const indexOrders = async (req: Request, res: Response) => {
-    const userId = req.body.userId
+    try {
+        const userId = req.body.userId
 
-    const orderList = await PaymentOrders.find({ userId })
+        const orderList = await PaymentOrders.find({ userId })
 
-    res.send({ success: true, data: orderList })
+        res.send({ success: true, data: orderList })
+    } catch (error) {
+        console.log(error)
+        throw new BadRequestError("Something went wrong")
+    }
+
 }
